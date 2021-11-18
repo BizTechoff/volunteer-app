9058aa0f8ff0a0d3be0da6f9705a7ca9507e05e9
Motidru <motidru@gmail.com>
2021-11-18 13:38:47 +0200

    v2.0.1

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -73,7 +73,7 @@ export const terms = {
     endSuccess: 'הסתיימו בהצלחה',
     problems: 'בעיות',
     invalidBranch: 'סניף אינו תקין',
-    appVersion: '2021.11.17.3',
+    appVersion: '2021.11.18.0',
     defaultVolunteers: 'מתנדבים ברירת מחדל',
     appDesc: 'אפליקציה לניהול מפגשי התנדבות',
     addVolunteer: 'הוספת מתנדב',
