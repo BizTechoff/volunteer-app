d96d76d37f870f4ad422f65dee9b26a320f40b82
Motidru <motidru@gmail.com>
2021-12-08 13:32:23 +0200

    v52

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -122,7 +122,7 @@ export const terms = {
     subTitle: 'כותרת משנית',
     volunteerApp: 'פעילות התנדבותית',
     currentStatus: 'סטטוס נוכחי',
-    currentState: 'מצב נוכחי',
+    currentState: 'גרף פעילות',
     calendar: 'יומן פעילות',
     activities: 'פעילויות',
     tenants: 'דיירים',
@@ -173,7 +173,7 @@ export const terms = {
     invalidSignIn: "פרטי כניסה שגויים",
     signOut: 'התנתקות',
     emailFormatError: 'פורמט ___@_.___ אמייל שגוי',
-    appVersion: '2021.12.07.1'
+    appVersion: '2021.12.08.0'
 }
 declare module 'remult' {
     export interface UserInfo {
