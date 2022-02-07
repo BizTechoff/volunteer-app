93585532835a3fb7ab9d4f3cb3b242cdad36080a
Motidru <motidru@gmail.com>
2022-02-07 10:50:24 +0200

    fix bug link

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -225,7 +225,7 @@ export const terms = {
     invalidSignIn: "פרטי כניסה שגויים",
     signOut: 'התנתקות',
     emailFormatError: 'פורמט ___@_.___ אמייל שגוי',
-    appVersion: '2022.02.06.0'
+    appVersion: '2022.02.07.0'
 }
 
 declare module 'remult' {
