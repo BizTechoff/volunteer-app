989d8d94d1befd2c3f06aa09b0a289e2121fcc35
Motidru <motidru@gmail.com>
2022-07-24 20:41:15 +0300

    calendar height 100%

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -229,7 +229,7 @@ export const terms = {
     invalidSignIn: "פרטי כניסה שגויים",
     signOut: 'התנתקות',
     emailFormatError: 'פורמט ___@_.___ אמייל שגוי',
-    appVersion: '2022.07.24.1'
+    appVersion: '2022.07.24.2'
 }
 
 declare module 'remult' {
